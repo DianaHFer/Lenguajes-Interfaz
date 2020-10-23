561d131c587f17cae1e4b389aacc32adff2eaf37
Diana Hernández Fernández <71353266+DianaHFer@users.noreply.github.com>
2020-10-23 16:28:44 -0700

    Create tipo3.c

---
+++ U2/2.1 Lectura y ejercicios de ARM32 del ebook OpenSource/ProgramasCapitulos/tipo3.c
@@ -0,0 +1,17 @@
+//  Código del programa tipos3.c
+//@@ Instituto Tecnologico de Tijuana
+//@@ Depto de Sistemas y Computación
+//@@ Ing.En Sistemas Computacionales
+
+//@@ Autor : Hernández Fernández Diana @DianaHFer
+//@@ Repositorio: https://github.com/DianaHFer/Ejercicios.git
+//@@ Fecha de revisión : Viernes 23 de Octubre del 2020.
+
+# include <stdio.h>
+
+void main ( void ){
+int i;
+for ( i= 0; i <5; i++ ){
+printf ( " %d\n " , i );
+}
+}
