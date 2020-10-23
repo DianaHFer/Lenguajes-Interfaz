3e03fc717bed14fbcfab7ec8632610d587c8ecf0
Diana Hernández Fernández <71353266+DianaHFer@users.noreply.github.com>
2020-10-23 16:32:06 -0700

    Update Tipos3b.s

--- U2/2.1 Lectura y ejercicios de ARM32 del ebook OpenSource/ProgramasCapitulos/Tipos3b.s
+++ U2/2.1 Lectura y ejercicios de ARM32 del ebook OpenSource/ProgramasCapitulos/Tipos3b.s
@@ -1,3 +1,11 @@
+// Código del programa tipos3b
+//@@ Instituto Tecnologico de Tijuana
+//@@ Depto de Sistemas y Computación
+//@@ Ing.En Sistemas Computacionales
+
+//@@ Autor : Hernández Fernández Diana @DianaHFer
+//@@ Repositorio: https://github.com/DianaHFer/Ejercicios.git
+//@@ Fecha de revisión : Viernes 23 de Octubre del 2020.
 .data
 var1: .asciz " %d\ 012 "
 
