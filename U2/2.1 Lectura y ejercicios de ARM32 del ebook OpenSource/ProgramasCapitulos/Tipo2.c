319317699a74b07dff9460be0a5e4807e87450f0
Diana Hernández Fernández <71353266+DianaHFer@users.noreply.github.com>
2020-10-23 16:26:41 -0700

    Create Tipo2.c

---
+++ U2/2.1 Lectura y ejercicios de ARM32 del ebook OpenSource/ProgramasCapitulos/Tipo2.c
@@ -0,0 +1,20 @@
+// Estructura if en C
+//@@ Instituto Tecnologico de Tijuana
+//@@ Depto de Sistemas y Computación
+//@@ Ing.En Sistemas Computacionales
+
+//@@ Autor : Hernández Fernández Diana @DianaHFer
+//@@ Repositorio: https://github.com/DianaHFer/Ejercicios.git
+//@@ Fecha de revisión : Viernes 23 de Octubre del 2020.
+# include <stdio.h>
+
+void main ( void ){
+
+int a , b;
+if( a == b ){
+/* Có digo entonces */
+}
+else {
+/* Có digo sino */
+}
+}
