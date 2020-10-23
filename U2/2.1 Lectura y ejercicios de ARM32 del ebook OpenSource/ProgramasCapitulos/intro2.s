b6f736714eb9bc8c0907d0fe154405ec8cca960b
Diana Hernández Fernández <71353266+DianaHFer@users.noreply.github.com>
2020-10-23 16:17:27 -0700

    Update intro2.s

--- U2/2.1 Lectura y ejercicios de ARM32 del ebook OpenSource/ProgramasCapitulos/intro2.s
+++ U2/2.1 Lectura y ejercicios de ARM32 del ebook OpenSource/ProgramasCapitulos/intro2.s
@@ -1,3 +1,12 @@
+  
+//  Código del programa intro2.s
+//@@ Instituto Tecnologico de Tijuana
+//@@ Depto de Sistemas y Computación
+//@@ Ing.En Sistemas Computacionales
+
+//@@ Autor : Hernández Fernández Diana @DianaHFer
+//@@ Repositorio: https://github.com/DianaHFer/Ejercicios.git
+//@@ Fecha de revisión : Viernes 23 de Octubre del 2020.
 .data
 var1: .byte 0b00110010
 		.align
