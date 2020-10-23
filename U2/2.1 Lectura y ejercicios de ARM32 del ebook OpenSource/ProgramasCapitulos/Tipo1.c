ffb4fe3083e10b2e5cbec908082a4a866a0cf1ed
Diana Hernández Fernández <71353266+DianaHFer@users.noreply.github.com>
2020-10-23 16:25:07 -0700

    Create Tipo1.c

---
+++ U2/2.1 Lectura y ejercicios de ARM32 del ebook OpenSource/ProgramasCapitulos/Tipo1.c
@@ -0,0 +1,23 @@
+//  Estructura del for y while en C 
+//@@ Instituto Tecnologico de Tijuana
+//@@ Depto de Sistemas y Computación
+//@@ Ing.En Sistemas Computacionales
+
+//@@ Autor : Hernández Fernández Diana @DianaHFer
+//@@ Repositorio: https://github.com/DianaHFer/Ejercicios.git
+//@@ Fecha de revisión : Viernes 23 de Octubre del 2020.
+
+# include <stdio.h>
+
+void main ( void ){
+
+int vi , vf , i;
+for ( i = vi; i <= vf; i++ ){
+/* Cuerpo del bucle */
+}
+i= vi;
+while ( i <= vf ){
+/* Cuerpo del bucle */
+i++;
+}
+}
